413171a8c590d50dd25a70139df54192d40bee69
Gong Cheng <512543469@qq.com>
2017-08-12 13:52:50 +0800

    Add files via upload

---
+++ panx-docker-add-new-pages-repo.sh
@@ -0,0 +1,13 @@
+﻿## Usage: bash add-new-pages-repo.sh your_species_prefix
+#species_prefix
+
+species=$1
+
+# process jade (in folder ./views/)
+#mkdir -p ./public/dataset/$species
+#mkdir -p ./public/dataset/$species/geneCluster
+cp ./views/instance.jade ./views/$species.jade
+sed -i -- "s/TestSet/$species/g" ./views/$species.jade
+# process js (in folder ./routes/)
+cp ./routes/TestSet.js ./routes/$species.js
+sed -i -- "s/TestSet/$species/g" ./routes/$species.js
---
+++ panx-docker-link-to-server.py
@@ -0,0 +1,9 @@
+﻿import os,sys
+species = sys.argv[1]
+def link_data_to_server():
+	"""
+	Sending json files and alignment to server for data visualization
+	"""
+	os.system('rsync -avz ./data/%s/meta-dict-%s.js /pan-genome-visualization/public/javascripts '%(species,species))
+	os.system('rsync -avz ./data/%s/vis/ /pan-genome-visualization/public/dataset/%s/ '%(species,species))
+link_data_to_server()
